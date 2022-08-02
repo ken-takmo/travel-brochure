914ae2ce45e2a2c5f01b3993da9c6708fab9dd75
kenichi <xianyigoben53@gmail.com>
2022-08-03 01:22:14 +0900

    詳細ページの作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { PostBrochure } from "./components/PostBrochure";
 import { GetBrochures } from "./components/GetBrochures";
 import { Header } from "./components/Header";
+import { Detail } from "./components/Detail";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/postbrochure" element={<PostBrochure />}></Route>
           <Route path="/getbrochures" element={<GetBrochures />}></Route>
+          <Route path="/detail/:id" element={<Detail />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Detail.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { db } from "../database/db";
+import { regions, companions } from "../utils/utils";
+export const Detail = () => {
+  const params = useParams();
+  const brochureID = params.id;
+  const [detail, setDetail] = useState([]);
+  const result = [];
+
+  useEffect(() => {
+    const getDetail = async () => {
+      const doc = await db.collection("trips").doc(brochureID).get();
+      result.push({ ...doc.data() });
+      console.log(result);
+      setDetail(result);
+    };
+    getDetail();
+  }, []);
+  return (
+    <main className="detail">
+      <h1>しおり詳細</h1>
+      {detail.map((deta) => {
+        return (
+          <div className="detail-data" key={brochureID}>
+            <div className="detail-data-header">
+              <h3>{deta.destination}</h3>
+            </div>
+            <hr />
+            <div className="detail-data-main">
+              <p className="detail-theme">旅行テーマ：{deta.theme}</p>
+              <p className="detail-content">感想：{deta.content}</p>
+            </div>
+            <hr />
+            <div className="detail-data-detail">
+              <p>評価：{deta.evaluation}</p>
+              <p>誰と：{companions[deta.companion]}</p>
+              <p>地域：{regions[deta.region]}</p>
+            </div>
+            <nav className="detail-data-links">
+              <p className="link"></p>
+              <p className="link"></p>
+            </nav>
+          </div>
+        );
+      })}
+    </main>
+  );
+};
