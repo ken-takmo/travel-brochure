cde8077353d5356e313f5e7b7ce23d5dea6cf68d
kenichi <xianyigoben53@gmail.com>
2022-08-21 00:11:36 +0900

    いいね機能実装

--- src/hooks/useGood.js
+++ src/hooks/useGood.js
@@ -1,48 +1,81 @@
-// import { db } from "../database/db";
+import { db } from "../database/db";
+import { useEffect, useState } from "react";
+import { collection, doc, setDoc, getDocs } from "firebase/firestore";
 
-// export const useGood = () => {
-//   const [isGood, setIsGood] = useState(false);
-//   const addGoodUser = async () => {
-//     try {
-//       await db.collection("trips").doc(id).collection("goodUser").add(user);
-//     } catch (err) {
-//       alert(err);
-//     }
-//   };
-//   const addEvaluation = async (id, evaluation, userId) => {
-//     try {
-//       await db
-//         .collection("trips")
-//         .doc(id)
-//         .update({
-//           evaluation: [evaluation + 1, userId],
-//         });
-//       await db
-//         .collection("trips")
-//         .doc(id)
-//         .collection("goods")
-//         .add({ user: userId });
-//     } catch (error) {
-//       alert(error);
-//     }
-//   };
-//   const reduceEvaluation = async (id, evaluation) => {
-//     if (evaluation == 0) {
-//       return;
-//     }
-//     try {
-//       await db
-//         .collection("trips")
-//         .doc(id)
-//         .update({
-//           evaluation: evaluation - 1,
-//         });
-//       // setIsGood(false);
-//     } catch (error) {
-//       alert(error);
-//     }
-//   };
-//   return {
-//     addEvaluation,
-//   };
-// };
+export const useGood = (id) => {
+  const [goodUsers, setGoodUSers] = useState([]);
+  const goodCollectionRef = collection(db, "trips", id, "goodUsers");
+
+  useEffect(() => {
+    const getGoodUser = async () => {
+      let users = [];
+      const snapShots = await getDocs(collection(db, "trips", id, "goodUsers"));
+      if (snapShots) {
+        snapShots.forEach((s) => {
+          users.push({ goodUserId: s.id, user: s.data().user });
+        });
+        setGoodUSers(users);
+      } else {
+        console.log("no");
+      }
+    };
+    getGoodUser();
+  }, []);
+  const addGoodUser = async (userId) => {
+    try {
+      await setDoc(doc(goodCollectionRef), {
+        user: userId,
+      });
+    } catch (err) {
+      alert(err);
+    }
+  };
+  const addEvaluation = async (evaluation, userId) => {
+    try {
+      await db
+        .collection("trips")
+        .doc(id)
+        .update({
+          evaluation: evaluation + 1,
+        });
+      addGoodUser(userId);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const deleteGoodUser = async (docId) => {
+    try {
+      await db
+        .collection("trips")
+        .doc(id)
+        .collection("goodUsers")
+        .doc(docId)
+        .delete();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const reduceEvaluation = async (evaluation, docId) => {
+    if (evaluation == 0) {
+      return;
+    }
+    try {
+      await db
+        .collection("trips")
+        .doc(id)
+        .update({
+          evaluation: evaluation - 1,
+        });
+      deleteGoodUser(docId);
+      console.log("delete");
+    } catch (error) {
+      alert(error);
+    }
+  };
+  return {
+    addEvaluation,
+    reduceEvaluation,
+    goodUsers,
+  };
+};
