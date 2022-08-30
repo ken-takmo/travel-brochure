97f79aac92c3c876c456fc4bad225ed122e0705d
kenichi <xianyigoben53@gmail.com>
2022-08-30 23:14:14 +0900

    ho-mkuno

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,9 @@ export const Home = () => {
         <img src={bookmarkimage} alt="説明1" className="description-img" />
         <div className="descriptions">
           <p>旅のしおりを共有するアプリです。</p>
-          <p>旅行先でのあなたの楽しみ方(テーマ)を教えてください。</p>
-          <p>魅力が詰まった素敵な写真を1枚投稿してください。</p>
+          <p>
+            魅力が詰まった素敵な写真を1枚とあなたの楽しみ方(旅のテーマ)を投稿してください
+          </p>
           <p>旅先×テーマでオリジナル旅のしおりを投稿しよう！</p>
         </div>
       </div>
