ca216685f5b282311d08b63f72f105bdf43803da
kenichi <xianyigoben53@gmail.com>
2022-10-23 00:31:13 +0900

    命名変更

--- src/pages/MyFavoriteBrochures.jsx
+++ src/pages/MyFavoriteBrochures.jsx
@@ -3,19 +3,19 @@ import { useAuth } from "../providers/AuthContext";
 import { BrochureData } from "../components/BrochureData";
 export const MyFavoriteBroshures = () => {
   const [isAuth] = useAuth();
-  const favoriteBrochuresData = useGetFavoriteBrochures(isAuth.uid);
-  console.log(favoriteBrochuresData);
+  const favoriteBrochuresDataList = useGetFavoriteBrochures(isAuth.uid);
+  console.log(favoriteBrochuresDataList);
 
   return (
     <main className="favorite-brochures">
       <h2>お気に入り</h2>
-      {favoriteBrochuresData.length == 0 ? (
+      {favoriteBrochuresDataList.length == 0 ? (
         <div className="no-content">
           <h3>お気に入りのしおりはまだありません</h3>
         </div>
       ) : (
         <div className="brochures">
-          {<BrochureData data={favoriteBrochuresData} />}
+          {<BrochureData data={favoriteBrochuresDataList} />}
         </div>
       )}
     </main>
