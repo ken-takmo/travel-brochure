bcfd9c8baf59f72f4d49b0fd2188870195d894e3
kenichi <xianyigoben53@gmail.com>
2022-10-24 17:48:06 +0900

    ４０４ページの追加

---
+++ src/pages/Page_404.jsx
@@ -0,0 +1,11 @@
+export const Page_404 = () => {
+  return (
+    <main className="page_404">
+      <h1>お探しのページは見つかりませんでした。</h1>
+      <div className="description-404">
+        <p>URLが間違っている可能性があります。</p>
+        <p>URLをご確認の上再度アクセスをお願いいたします。</p>
+      </div>
+    </main>
+  );
+};
