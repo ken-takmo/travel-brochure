70e6e52cd0869e282b51ac4e3d532b8cb4c73aee
kenichi <xianyigoben53@gmail.com>
2022-10-24 19:25:54 +0900

    存在しない投稿検索時の処理追加

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -20,89 +20,98 @@ export const Detail = () => {
   }, [detail.evaluation]);
   return (
     <main className="detail">
-      <div className="detail-data" key={brochureID}>
-        <div className="detail-data-header">
-          <h3>{detail.destination}</h3>
+      {!Object.keys(detail).length ? (
+        <div>
+          <p>お探しの投稿はありません。</p>
         </div>
-        <hr />
-        <div className="detail-data-main">
-          <div className="image">
-            <Image url={detail.image} />
+      ) : (
+        <div className="detail-data" key={brochureID}>
+          <div className="detail-data-header">
+            <h3>{detail.destination}</h3>
           </div>
-          <div className="detail-theme">
-            <h3>テーマ</h3>
-            <p>{detail.theme}</p>
+          <hr />
+          <div className="detail-data-main">
+            <div className="image">
+              <Image url={detail.image} />
+            </div>
+            <div className="detail-theme">
+              <h3>テーマ</h3>
+              <p>{detail.theme}</p>
+            </div>
+            <div className="detail-content">
+              <h3>内容</h3>
+              <p>{detail.content}</p>
+            </div>
           </div>
-          <div className="detail-content">
-            <h3>内容</h3>
-            <p>{detail.content}</p>
+          <hr />
+          <div className="detail-data-detail">
+            {isAuth ? (
+              <>
+                {isGood ? (
+                  <>
+                    <span
+                      className="material-symbols-rounded favorited"
+                      onClick={() => {
+                        setGoodCount(goodCount - 1);
+                        reduceEvaluation(detail.evaluation, goodCount - 1);
+                      }}
+                    >
+                      favorite
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <span
+                      className="material-symbols-rounded favorite"
+                      onClick={() => {
+                        setGoodCount(goodCount + 1);
+                        addEvaluation(detail.evaluation, goodCount + 1);
+                      }}
+                    >
+                      favorite
+                    </span>
+                  </>
+                )}
+                <div className="good">
+                  <p onClick={() => getFavoriteUsers()}>{goodCount}</p>
+                </div>
+                <p>誰と：{companions[detail.companion]}</p>
+                <p>地域：{regions[detail.region]}</p>
+              </>
+            ) : (
+              <>
+                <div className="good">
+                  <p onClick={() => getFavoriteUsers()}>{goodCount} いいね！</p>
+                </div>
+                <p>誰と：{companions[detail.companion]}</p>
+                <p>地域：{regions[detail.region]}</p>
+              </>
+            )}
           </div>
+          <nav className="detail-data-links">
+            {isAuth ? (
+              <>
+                {isAuth.uid === detail.userId ? (
+                  <>
+                    <EditButton
+                      url={`/updateform/${brochureID}`}
+                      text={"編集"}
+                    />
+                    <EditButton url={"/list"} text={"戻る"} />
+                    <button onClick={() => deleteBrochure(detail.image)}>
+                      削除
+                    </button>
+                  </>
+                ) : (
+                  <EditButton url={-1} text={"戻る"} />
+                )}
+              </>
+            ) : (
+              <EditButton url={-1} text={"戻る"} />
+            )}
+          </nav>
         </div>
-        <hr />
-        <div className="detail-data-detail">
-          {isAuth ? (
-            <>
-              {isGood ? (
-                <>
-                  <span
-                    className="material-symbols-rounded favorited"
-                    onClick={() => {
-                      setGoodCount(goodCount - 1);
-                      reduceEvaluation(detail.evaluation, goodCount - 1);
-                    }}
-                  >
-                    favorite
-                  </span>
-                </>
-              ) : (
-                <>
-                  <span
-                    className="material-symbols-rounded favorite"
-                    onClick={() => {
-                      setGoodCount(goodCount + 1);
-                      addEvaluation(detail.evaluation, goodCount + 1);
-                    }}
-                  >
-                    favorite
-                  </span>
-                </>
-              )}
-              <div className="good">
-                <p onClick={() => getFavoriteUsers()}>{goodCount}</p>
-              </div>
-              <p>誰と：{companions[detail.companion]}</p>
-              <p>地域：{regions[detail.region]}</p>
-            </>
-          ) : (
-            <>
-              <div className="good">
-                <p onClick={() => getFavoriteUsers()}>{goodCount} いいね！</p>
-              </div>
-              <p>誰と：{companions[detail.companion]}</p>
-              <p>地域：{regions[detail.region]}</p>
-            </>
-          )}
-        </div>
-        <nav className="detail-data-links">
-          {isAuth ? (
-            <>
-              {isAuth.uid === detail.userId ? (
-                <>
-                  <EditButton url={`/updateform/${brochureID}`} text={"編集"} />
-                  <EditButton url={"/list"} text={"戻る"} />
-                  <button onClick={() => deleteBrochure(detail.image)}>
-                    削除
-                  </button>
-                </>
-              ) : (
-                <EditButton url={-1} text={"戻る"} />
-              )}
-            </>
-          ) : (
-            <EditButton url={-1} text={"戻る"} />
-          )}
-        </nav>
-      </div>
+      )}
     </main>
   );
 };
